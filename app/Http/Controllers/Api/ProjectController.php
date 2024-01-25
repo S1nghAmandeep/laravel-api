4d0eb4ed7e38d2731a2ba7c8cc7eb295e01e68d6
SinghAmandeep <amandeepsingh3095@gmail.com>
2024-01-25 15:44:21 +0100

    add store method from api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+
 
 class ProjectController extends Controller
 {
@@ -29,5 +32,45 @@ class ProjectController extends Controller
             'success' => 'true',
             'results' => $project
         ]);
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'title' => 'required|max:50|string|',
+            'description' => 'nullable|string',
+            'technologies' => 'exists:technologies,id',
+            'cover_image' => 'nullable|max:2048|file'
+        ]);
+
+        $data = $request->all();
+        $data['slug'] = Str::slug($data['title'], '-');
+
+        if ($request->has('cover_image')) {
+            $img_path = Storage::put('upload', $request->cover_image);
+
+            $data['cover_image'] = $img_path;
+        }
+
+        $new_project = Project::create($data);
+
+        if ($request->has('technologies')) {
+            $new_project->technologies()->attach($data['technologies']);
+        }
+
+        if ($new_project) {
+
+            return response()->json([
+                'status' => 200,
+                'message' => $new_project
+            ], 200);
+        } else {
+
+            return response()->json([
+                'status' => 500,
+                'message' => 'failed'
+            ], 500);
+        }
     }
 }
